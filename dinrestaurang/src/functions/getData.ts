bfe923a75eb96372d9f1bda9b125f484a9c84771
FadiHanna <fadi.hanna@rektron.se>
2022-06-09 22:42:01 +0200

    Add debug and fix Eslint code

--- dinrestaurang/src/functions/getData.ts
+++ dinrestaurang/src/functions/getData.ts
@@ -1,16 +1,18 @@
-import { Food } from 'models';
+import { Food } from "models";
 
-import { getFoodData } from './api';
+import { getFoodData } from "./api";
 
 export const getData = async (
-  setLoading: (loading: boolean) => void,
-  setList: (list: Food[]) => void,
-  setError: (error: string) => void
+    setLoading: (loading: boolean) => void,
+    setList: (list: Food[]) => void,
+    setError: (error: string) => void
 ): Promise<void> => {
-  if (setLoading) setLoading(true);
+    if (setLoading) {
+        setLoading(true);
+    }
 
-  getFoodData()
-    .then((data: any) => setList && setList(data))
-    .catch((err: Error) => setError && setError(err.message))
-    .finally(() => setLoading && setLoading(false));
+    getFoodData()
+        .then((data: any) => setList && setList(data))
+        .catch((err: Error) => setError && setError(err.message))
+        .finally(() => setLoading && setLoading(false));
 };
