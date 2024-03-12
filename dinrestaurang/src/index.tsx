d22d74698dee1966854534f7dfd8a59d01d52bec
Fadi Hanna <54924938+fadihanna123@users.noreply.github.com>
2024-03-12 22:05:38 +0100

    Rename env values

--- dinrestaurang/src/index.tsx
+++ dinrestaurang/src/index.tsx
@@ -8,13 +8,13 @@ import { store } from './redux/app/store';
 import { GlobalStyles } from 'styles';
 import { toast } from 'react-toastify';
 
-const { REACT_APP_APIKEY, REACT_APP_AUTHORIZATION } = process.env;
+const { REACT_APP_API_KEY, REACT_APP_AUTHORIZATION_KEY } = process.env;
 
 if (process.env.NODE_ENV === 'development') {
   console.log("It's looks like that we are in a development mode!");
 }
 
-if (!REACT_APP_APIKEY || !REACT_APP_AUTHORIZATION) {
+if (!REACT_APP_API_KEY || !REACT_APP_AUTHORIZATION_KEY) {
   toast.error('Missing keys! Add them and restart the app.');
   throw new Error('Missing keys! Add them and restart the app.');
 }
