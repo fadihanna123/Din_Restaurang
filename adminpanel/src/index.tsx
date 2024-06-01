24bdd2d2fc5a0cbe6a73dd3519f5ac084d2a31af
Fadi Hanna <54924938+fadihanna123@users.noreply.github.com>
2024-06-01 21:02:38 +0200

    Optimize Docker config and update adminpanel envs

--- adminpanel/src/index.tsx
+++ adminpanel/src/index.tsx
@@ -7,15 +7,16 @@ import { Provider } from 'react-redux';
 import { store } from './redux/app/store';
 import GlobalStyles from 'styles/global';
 import { toast } from 'react-toastify';
+import { apiKey, authorizationKey } from './utils';
 
-const { NODE_ENV, REACT_APP_APIKEY, REACT_APP_AUTHORIZATION } = process.env;
+const { NODE_ENV } = process.env;
 
 if (NODE_ENV === 'development') {
   // eslint-disable-next-line no-console
   console.log("It's looks like we are in a development mode");
 }
 
-if (!REACT_APP_APIKEY || !REACT_APP_AUTHORIZATION) {
+if (!apiKey || !authorizationKey) {
   toast.error('Missing keys! Add them and restart the app.');
   throw new Error('Missing keys! Add them and restart the app.');
 }
--- adminpanel/src/utils/envsVariables.ts
+++ adminpanel/src/utils/envsVariables.ts
@@ -1,11 +1,11 @@
 const {
   REACT_APP_BACKEND_URL,
-  REACT_APP_API_KEY,
-  REACT_APP_AUTHORIZATION_KEY,
+  REACT_APP_APIKEY,
+  REACT_APP_AUTHORIZATION,
 } = process.env;
 
 export const backendURL = REACT_APP_BACKEND_URL;
 
-export const apiKey = REACT_APP_API_KEY;
+export const apiKey = REACT_APP_APIKEY;
 
-export const authorizationKey = REACT_APP_AUTHORIZATION_KEY;
+export const authorizationKey = REACT_APP_AUTHORIZATION;
